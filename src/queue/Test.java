d336c9d41c23d67d12d13ba8d3fca4b15bb90ae3
杨育才 <1127424957@qq.com>
2018-09-28 23:20:05 +0800

    测试相关

---
+++ src/queue/Test.java
@@ -0,0 +1,68 @@
+package queue;
+
+import java.util.Random;
+
+public class Test {
+
+    public static void main(String[] args){
+//        test_ArrayQueue();
+//        test_LoopQueue();
+        test_time();
+    }
+
+    public static void test_time() {
+        _ArrayQueue<Integer> arrayQueue = new _ArrayQueue<>();
+        _LoopQueue<Integer> loopQueue = new _LoopQueue<>();
+
+        int count = 1000000;
+
+        double time1 = computeTime(arrayQueue, count);
+        double time2 = computeTime(loopQueue, count);
+
+        System.out.println("_ArrayQueue: " + time1);
+        System.out.println("_LoopQueue: " + time2);
+    }
+
+    public static double computeTime(_Queue<Integer> q, int count) {
+        Random random = new Random();
+
+        long start = System.nanoTime();
+
+        for(int i = 0; i < count; i ++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+
+        for(int i = 0; i < count; i ++) {
+            q.dequeue();
+        }
+
+        long end = System.nanoTime();
+
+        return (end - start) / 1000000000.0;
+    }
+
+    public static void test_ArrayQueue() {
+        _ArrayQueue<Integer> queue = new _ArrayQueue<>();
+
+        for(int i = 0; i < 5; i ++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+        }
+
+        queue.dequeue();
+        System.out.println(queue);
+    }
+
+    public static void test_LoopQueue() {
+        _LoopQueue<Integer> queue = new _LoopQueue<>();
+
+        for(int i = 0; i < 3; i ++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+        }
+
+        queue.dequeue();
+        System.out.println(queue);
+    }
+
+}
