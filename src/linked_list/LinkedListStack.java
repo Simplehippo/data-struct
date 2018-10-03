66cdb4415632705388e3b38205f26ad4b17eea29
杨育才 <1127424957@qq.com>
2018-10-03 22:10:14 +0800

    增加链表实现的栈

---
+++ src/linked_list/LinkedListStack.java
@@ -0,0 +1,39 @@
+package linked_list;
+
+import stack._Stack;
+
+public class _LinkedListStack<E> implements _Stack<E> {
+
+    private _LinkedList<E> data;
+    private int size;
+
+    public _LinkedListStack() {
+        this.data = new _LinkedList<>();
+        size = 0;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void push(E e) {
+        data.addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return data.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return data.get(0);
+    }
+}
