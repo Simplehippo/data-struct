0d80809ab144730e00a6fe3a26422471a734541d
杨育才 <1127424957@qq.com>
2018-10-06 15:47:32 +0800

    实现带虚拟头和尾指针的单向链表

---
+++ src/linked_list/_TailLinkedList.java
@@ -0,0 +1,155 @@
+package linked_list;
+
+public class _TailLinkedList<E> {
+    private Node dummyHead;
+    private Node tail;
+    private int size;
+
+    public _TailLinkedList() {
+        dummyHead = new Node(null, null);
+        tail = dummyHead;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(int index, E e) {
+        if(index < 0 || index > size) {
+            throw new IllegalArgumentException("add: index out!");
+        }
+
+        Node prev = dummyHead;
+        for(int i=0; i<index; i++) {
+            prev = prev.next;
+        }
+
+        Node newNode = new Node(e, prev.next);
+        prev.next = newNode;
+        if(index == size) {
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        Node newNode = new Node(e, tail.next);
+        tail.next = newNode;
+        tail = newNode;
+        size++;
+    }
+
+    public E remove(int index) {
+        if(index < 0 || index >= size) {
+            throw new IllegalArgumentException("remove: index out!");
+        }
+
+        Node prev = dummyHead;
+        for(int i=0; i<index; i++) {
+            prev = prev.next;
+        }
+
+        Node removeNode = prev.next;
+        prev.next = removeNode.next;
+        if(index == size - 1) {
+            tail = prev;
+        }
+
+        size--;
+        return removeNode.e;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public int find(E e) {
+        Node cur = dummyHead.next;
+        for(int i=0; i<size; i++) {
+            if(e==null) {
+                if(cur.e == null) {
+                    return i;
+                }
+            } else if(e.equals(cur.e)) {
+                return i;
+            }
+            cur = cur.next;
+        }
+        return -1;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        for(int i=0; i<size; i++) {
+            if(e==null) {
+                if(cur.e == null) {
+                    return true;
+                }
+            } else if(e.equals(cur.e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public E get(int index) {
+        if(index < 0 || index >= size) {
+            throw new IllegalArgumentException("get: index out!");
+        }
+
+        Node cur = dummyHead.next;
+        for(int i=0; i<index; i++) {
+            cur = cur.next;
+        }
+
+        return cur.e;
+    }
+
+    public void set(int index, E e) {
+        if(index < 0 || index >= size) {
+            throw new IllegalArgumentException("set: index out!");
+        }
+
+        Node cur = dummyHead.next;
+        for(int i=0; i<index; i++) {
+            cur = cur.next;
+        }
+
+        cur.e = e;
+    }
+
+
+
+    private class Node {
+        E e;
+        Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this.e = e;
+        }
+    }
+
+
+
+
+
+}
